3911c55a9d2f7e3d5e2f6546035426c8c11e0d6f
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-09-24 19:26:25 +0200

    Movimiento programado. Queda afinar el algoritmo pero la nave se mueve (aunque de forma rudimentaria)

--- app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.android.spaceinvaders;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void actualizaPosicionIzq(View view){
+        findViewById(R.id.nave).setX(findViewById(R.id.nave).getX()-30);
+    }
+
+    public void actualizaPosicionDer(View view){
+        findViewById(R.id.nave).setX(findViewById(R.id.nave).getX()+30);
     }
 }
