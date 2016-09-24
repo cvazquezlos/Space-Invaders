be38edd4d13ab5acae2afbc2cc4bbcb90a5cb009
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-09-24 23:01:41 +0200

    Algoritmo refinado.

--- app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.spaceinvaders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void actualizaPosicionIzq(View view) {
+        Button botonIzq = (Button) view;
         findViewById(R.id.nave).setX(findViewById(R.id.nave).getX() + 30);
     }
 
     public void actualizaPosicionDer(View view) {
+        Button botonDer = (Button) view;
         findViewById(R.id.nave).setX(findViewById(R.id.nave).getX() - 30);
     }
 }
