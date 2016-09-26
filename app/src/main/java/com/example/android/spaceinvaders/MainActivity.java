073c171994f6ac7ab1a768856166e87d19465d3a
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-09-26 18:28:45 +0200

    La nave dispara sin control de pulsación ni desaparece al llegar al principio de la pantalla

--- app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.android.spaceinvaders;
 
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    TimerTask tarea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tarea = new TimerTask() {
+            @Override
+            public void run() {
+                findViewById(R.id.municion).setY(findViewById(R.id.municion).getY() - 50);
+            }
+        };
     }
 
     public void actualizaPosicion(View v) {
@@ -21,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.nave).setX(findViewById(R.id.nave).getX() + 30);
                 break;
         }
+    }
+
+    public void dispara(View v) {
+        ImageView municion = (ImageView) findViewById(R.id.municion);
+        municion.setVisibility(View.VISIBLE);
+        Timer timer = new Timer();
+        timer.schedule(tarea, 0, 500);
+    }
+
+    private boolean llegaAlFinal(ImageView municion) {
+        //return municion.getY()==;
+        return true;
     }
 }
