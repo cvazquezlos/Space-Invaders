89b4219954633b52847ed10e18abf9e6b936a4ac
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-09-28 19:38:31 +0200

    Algoritmo editado. Eliminada la redundancia por variables de clase. Imagen del láser de la nave modificada y mejorada.

--- app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.android.spaceinvaders;
 
-import android.os.Handler;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView municion;
-    RelativeLayout rl;
+    ImageView nave;
+    Button botonDisparo;
     Handler manejaDisparo = new Handler();
     final int movimiento = 30;
 
@@ -19,32 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         municion = (ImageView) findViewById(R.id.municion);
-        rl = (RelativeLayout) findViewById(R.id.activity_main);
+        nave = (ImageView) findViewById(R.id.nave);
+        botonDisparo = (Button) findViewById(R.id.disparo);
     }
 
     public void actualizaPosicion(View v) {
         switch (v.getId()) {
             case (R.id.control_derecha):
-                if (!seSale("der")) {
-                    findViewById(R.id.nave).setX(findViewById(R.id.nave).getX() - movimiento);
-                }
+                if (!seSale("der"))
+                    nave.setX(nave.getX() - movimiento);
                 break;
             case R.id.control_izquierda:
-                if (!seSale("izq")) {
-                    findViewById(R.id.nave).setX(findViewById(R.id.nave).getX() + movimiento);
-                }
+                if (!seSale("izq"))
+                    nave.setX(nave.getX() + movimiento);
                 break;
         }
     }
 
-    private boolean seSale(String direccion) {
-        switch (direccion) {
-            case "izq":
-                return (findViewById(R.id.nave).getX() + movimiento + findViewById(R.id.nave).getWidth()) > rl.getWidth();
-            case "der":
-                return (findViewById(R.id.nave).getX() - movimiento) < 0;
-        }
-        return true;
+    public void dispara(View v) {
+        municion.setX(nave.getX() + (((nave.getWidth()) / 2) - 15));
+        municion.setY(nave.getY());
+        municion.setVisibility(View.VISIBLE);
+        botonDisparo.setEnabled(false);
+        manejaDisparo.postDelayed(accionDisparo, 0);
     }
 
     Runnable accionDisparo = new Runnable() {
@@ -54,21 +52,23 @@ public class MainActivity extends AppCompatActivity {
             if (llegaAlFinal()) {
                 municion.setVisibility(View.INVISIBLE);
                 manejaDisparo.removeCallbacks(accionDisparo);
-                findViewById(R.id.disparo).setEnabled(true);
+                botonDisparo.setEnabled(true);
             }
             manejaDisparo.postDelayed(this, 80);
         }
     };
 
-    public void dispara(View v) {
-        municion.setX(findViewById(R.id.nave).getX() + (((findViewById(R.id.nave).getWidth()) / 2) - 15));
-        municion.setY(findViewById(R.id.nave).getY());
-        municion.setVisibility(View.VISIBLE);
-        findViewById(R.id.disparo).setEnabled(false);
-        manejaDisparo.postDelayed(accionDisparo, 0);
+    private boolean llegaAlFinal() {
+        return municion.getY() <= 50;
     }
 
-    private boolean llegaAlFinal() {
-        return findViewById(R.id.municion).getY() <= 50;
+    private boolean seSale(String direccion) {
+        switch (direccion) {
+            case "izq":
+                return (nave.getX() + movimiento + nave.getWidth()) > findViewById(R.id.activity_main).getWidth();
+            case "der":
+                return (nave.getX() - movimiento) < 0;
+        }
+        return true;
     }
 }
