49612ec2c7214ea7f2b8cc21d9c6c6a7bfa2e1dd
Carlos <Carlos>
2016-09-28 15:27:08 +0200

    Actividad de disparo arreglada. Sustituido el TimerTask por un Runnable en la misma clase de la actividad.

--- app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
@@ -1,21 +1,17 @@
 package com.example.android.spaceinvaders;
 
+import android.os.Handler;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class MainActivity extends AppCompatActivity {
 
-    TimerTask tarea;
-    Timer timer;
-    float posicionInicialMunicion;
     ImageView municion;
     RelativeLayout rl;
+    Handler manejaDisparo = new Handler();
     final int movimiento = 30;
 
     @Override
@@ -24,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         municion = (ImageView) findViewById(R.id.municion);
         rl = (RelativeLayout) findViewById(R.id.activity_main);
-        posicionInicialMunicion = municion.getY();
     }
 
     public void actualizaPosicion(View v) {
@@ -35,52 +30,45 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.control_izquierda:
-                if (!seSale("izq")){
+                if (!seSale("izq")) {
                     findViewById(R.id.nave).setX(findViewById(R.id.nave).getX() + movimiento);
                 }
                 break;
         }
     }
 
-    private boolean seSale(String direccion){
-        switch (direccion){
+    private boolean seSale(String direccion) {
+        switch (direccion) {
             case "izq":
-                return (findViewById(R.id.nave).getX() + movimiento + findViewById(R.id.nave).getWidth())>rl.getWidth();
+                return (findViewById(R.id.nave).getX() + movimiento + findViewById(R.id.nave).getWidth()) > rl.getWidth();
             case "der":
                 return (findViewById(R.id.nave).getX() - movimiento) < 0;
         }
         return true;
     }
 
-    public void dispara(View v) {
-        ImageView ammo = (ImageView) findViewById(R.id.municion);
-        if (ammo.getY() >= posicionInicialMunicion) {
-            System.out.println(findViewById(R.id.municion).getY() + "\n\n\n");
-            ammo.setVisibility(View.VISIBLE);
-            timer = new Timer();
-            tarea = new TimerTask() {
-                @Override
-                public void run() {
-                    if (!llegaAlFinal()) {
-                        municion.setY(municion.getY() - 50);
-                        System.out.println(municion.getY() + "\n");
-                    } else
-                        restauraPorDefecto();
-
-                }
-            };
+    Runnable accionDisparo = new Runnable() {
+        @Override
+        public void run() {
+            municion.setY(municion.getY() - 50);
+            if (llegaAlFinal()) {
+                municion.setVisibility(View.INVISIBLE);
+                manejaDisparo.removeCallbacks(accionDisparo);
+                findViewById(R.id.disparo).setEnabled(true);
+            }
+            manejaDisparo.postDelayed(this, 80);
         }
-        timer.schedule(tarea, 0, 250);
+    };
+
+    public void dispara(View v) {
+        municion.setX(findViewById(R.id.nave).getX() + (((findViewById(R.id.nave).getWidth()) / 2) - 15));
+        municion.setY(findViewById(R.id.nave).getY());
+        municion.setVisibility(View.VISIBLE);
+        findViewById(R.id.disparo).setEnabled(false);
+        manejaDisparo.postDelayed(accionDisparo, 0);
     }
 
     private boolean llegaAlFinal() {
         return findViewById(R.id.municion).getY() <= 50;
-    }
-
-    private void restauraPorDefecto() {
-        timer.cancel();
-        tarea.cancel();
-        municion.setX(findViewById(R.id.nave).getX() + (findViewById(R.id.nave).getWidth()) / 2);
-        municion.setY(posicionInicialMunicion);
     }
 }
