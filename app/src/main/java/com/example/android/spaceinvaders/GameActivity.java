56e1a71a198dccf196045d36fc8c38885713f167
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-10-12 18:22:54 +0200

    Actividades referenciadas correctamente. Manifiesto xml modificado.

---
+++ app/src/main/java/com/example/android/spaceinvaders/GameActivity.java
@@ -0,0 +1,78 @@
+package com.example.android.spaceinvaders;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class GameActivity extends AppCompatActivity {
+
+    ImageView municion;
+    ImageView nave;
+    Button botonDisparo;
+    Handler manejaDisparo = new Handler();
+    final int movimiento = 30;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+        municion = (ImageView) findViewById(R.id.municion);
+        nave = (ImageView) findViewById(R.id.nave);
+        botonDisparo = (Button) findViewById(R.id.disparo);
+    }
+
+    public void actualizaPosicion(View v) {
+        switch (v.getId()) {
+            case (R.id.control_derecha):
+                if (!seSale("der")) {
+                    nave.setImageResource(R.drawable.diseno12);
+                    nave.setX(nave.getX() - movimiento);
+                }
+                break;
+            case R.id.control_izquierda:
+                if (!seSale("izq")) {
+                    nave.setImageResource(R.drawable.diseno13);
+                    nave.setX(nave.getX() + movimiento);
+                }
+                break;
+        }
+    }
+
+    public void dispara(View v) {
+        nave.setImageResource(R.drawable.diseno11);
+        municion.setX(nave.getX() + (((nave.getWidth()) / 2) - 5));
+        municion.setY(nave.getY());
+        municion.setVisibility(View.VISIBLE);
+        botonDisparo.setEnabled(false);
+        manejaDisparo.postDelayed(accionDisparo, 0);
+    }
+
+    Runnable accionDisparo = new Runnable() {
+        @Override
+        public void run() {
+            municion.setY(municion.getY() - 50);
+            if (llegaAlFinal()) {
+                municion.setVisibility(View.INVISIBLE);
+                manejaDisparo.removeCallbacks(accionDisparo);
+                botonDisparo.setEnabled(true);
+            }
+            manejaDisparo.postDelayed(this, 80);
+        }
+    };
+
+    private boolean llegaAlFinal() {
+        return municion.getY() <= 20;
+    }
+
+    private boolean seSale(String direccion) {
+        switch (direccion) {
+            case "izq":
+                return (nave.getX() + movimiento + nave.getWidth()) > findViewById(R.id.activity_main).getWidth();
+            case "der":
+                return (nave.getX() - movimiento) < 0;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/android/spaceinvaders/MainActivity.java
@@ -1,79 +1,30 @@
 package com.example.android.spaceinvaders;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageView municion;
-    ImageView nave;
-    Button botonDisparo;
-    Handler manejaDisparo = new Handler();
-    final int movimiento = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        municion = (ImageView) findViewById(R.id.municion);
-        nave = (ImageView) findViewById(R.id.nave);
-        botonDisparo = (Button) findViewById(R.id.disparo);
-    }
 
-    public void actualizaPosicion(View v) {
-        switch (v.getId()) {
-            case (R.id.control_derecha):
-                if (!seSale("der")) {
-                    nave.setImageResource(R.drawable.diseno12);
-                    nave.setX(nave.getX() - movimiento);
+        ImageButton playBoton = (ImageButton) findViewById(R.id.play_boton);
+        playBoton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent gameScreen = new Intent(view.getContext(), GameActivity.class);
+                startActivityForResult(gameScreen, 0);
+                try {
+                    this.finalize();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
                 }
-                break;
-            case R.id.control_izquierda:
-                if (!seSale("izq")) {
-                    nave.setImageResource(R.drawable.diseno13);
-                    nave.setX(nave.getX() + movimiento);
-                }
-                break;
-        }
-    }
-
-    public void dispara(View v) {
-        nave.setImageResource(R.drawable.diseno11);
-        municion.setX(nave.getX() + (((nave.getWidth()) / 2) - 5));
-        municion.setY(nave.getY());
-        municion.setVisibility(View.VISIBLE);
-        botonDisparo.setEnabled(false);
-        manejaDisparo.postDelayed(accionDisparo, 0);
-    }
-
-    Runnable accionDisparo = new Runnable() {
-        @Override
-        public void run() {
-            municion.setY(municion.getY() - 50);
-            if (llegaAlFinal()) {
-                municion.setVisibility(View.INVISIBLE);
-                manejaDisparo.removeCallbacks(accionDisparo);
-                botonDisparo.setEnabled(true);
             }
-            manejaDisparo.postDelayed(this, 80);
-        }
-    };
-
-    private boolean llegaAlFinal() {
-        return municion.getY() <= 20;
+        });
     }
 
-    private boolean seSale(String direccion) {
-        switch (direccion) {
-            case "izq":
-                return (nave.getX() + movimiento + nave.getWidth()) > findViewById(R.id.activity_main).getWidth();
-            case "der":
-                return (nave.getX() - movimiento) < 0;
-        }
-        return true;
-    }
 }
